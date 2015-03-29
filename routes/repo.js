80ff242099147b605df4ab683f5ee4de905bb72f
Vivan <vivangkumar@gmail.com>
2015-03-29 18:19:53 +0100

    Check for empty string in search, style fixes

--- routes/repo.js
+++ routes/repo.js
@@ -10,29 +10,33 @@ router.get('/', function(req, res, next) {
 
 router.get('/search', function(req, res, next) {
   var query = req.query.repo;
-  var request = new Request(
-    '/search/repositories?q='+query+'+in:name+user:' + config.githubUser,
-    'GET',
-    {'Authorization': 'token ' + req.signedCookies.accessToken},
-    null
-  );
+  if (query == '') {
+    res.render('repo', {error: 'Please enter a search term'});
+  } else {
+    var request = new Request(
+      '/search/repositories?q='+query+'+in:name+user:' + config.githubUser,
+      'GET',
+      {'Authorization': 'token ' + req.signedCookies.accessToken},
+      null
+    );
 
-  request.do(function(error, response, body) {
-    if (error) {
-      res.status(500).send(JSON.stringify({"error": error}))
-    }
-
-    if (response.statusCode == 200) {
-      var repoNames = [];
-      var parsedRepos = JSON.parse(body).items;
-      for(var i = 0; i < parsedRepos.length; i++) {
-        repoNames.push(parsedRepos[i].name);
+    request.do(function(error, response, body) {
+      if (error) {
+        res.status(500).send(JSON.stringify({"error": error}))
       }
-      res.render('repo', {repoNames: repoNames});
-    } else {
-      res.status(response.statusCode).send(body);
-    }
-  });
+
+      if (response.statusCode == 200) {
+        var repoNames = [];
+        var parsedRepos = JSON.parse(body).items;
+        for(var i = 0; i < parsedRepos.length; i++) {
+          repoNames.push(parsedRepos[i].name);
+        }
+        res.render('repo', {repoNames: repoNames});
+      } else {
+        res.status(response.statusCode).send(body);
+      }
+    });
+  }
 });
 
 module.exports = router;
