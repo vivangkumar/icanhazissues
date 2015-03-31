7b69eed21945d887835e76da3ae80e1c44f482ba
Vivan Kumar <vivangkumar@gmail.com>
2015-03-31 20:09:53 +0100

    Redirect erros to error page, remove quotes for JSON keys

--- routes/issues.js
+++ routes/issues.js
@@ -8,29 +8,34 @@ router.post('/:repo/update/:issue', function(req, res, next) {
   var issueNumber = req.params.issue
     , repoName = req.params.repo;
   var body = {
-    'labels': [req.body.newLabel]
+    labels: [req.body.newLabel]
   };
   var request = new Request(
     '/repos/' + config.githubUser + '/' + repoName + '/issues/' + issueNumber,
     'PATCH',
-    {'Authorization': 'token ' + req.signedCookies.accessToken},
+    {Authorization: 'token ' + req.signedCookies.accessToken},
     body
   );
 
   request.do(function(error, response, body) {
     if (error) {
-      res.status(500).send(JSON.stringify({'error': error}))
+      res.render('error', {
+        message: error,
+        error: {
+          status: 500
+        }
+      });
     }
 
     if (response.statusCode == 200) {
-      res.status(200).send(JSON.stringify({'message': 'Issue updated'}));
+      res.status(200).send(JSON.stringify({message: 'Issue updated'}));
       var commentData = {
-        'owner': config.githubUser,
-        'repo': repoName,
-        'issueNumber': issueNumber,
-        'accessToken': req.signedCookies.accessToken,
-        'oldLabel': req.body.oldLabel,
-        'newLabel': req.body.newLabel
+        owner: config.githubUser,
+        repo: repoName,
+        issueNumber: issueNumber,
+        accessToken: req.signedCookies.accessToken,
+        oldLabel: req.body.oldLabel,
+        newLabel: req.body.newLabel
       };
       try {
         _postIssueComment(commentData);
@@ -38,7 +43,12 @@ router.post('/:repo/update/:issue', function(req, res, next) {
         console.log(ex);
       }
     } else {
-      res.status(response.statusCode).send(body);
+      res.render('error', {
+        message: JSON.parse(body).message,
+        error: {
+          status: response.statusCode,
+        }
+      });
     }
   });
 });
@@ -51,8 +61,8 @@ function _postIssueComment(data) {
   var request = new Request(
     '/repos/' + data.owner + '/' + data.repo + '/issues/' + data.issueNumber + '/comments',
     'POST',
-    {'Authorization': 'token ' + data.accessToken},
-    {'body': 'changed status from ' + data.oldLabel + ' to ' + data.newLabel}
+    {Authorization: 'token ' + data.accessToken},
+    {body: 'changed status from ' + data.oldLabel + ' to ' + data.newLabel}
   );
 
   request.do(function(error, response, body) {
--- routes/repo.js
+++ routes/repo.js
@@ -14,26 +14,36 @@ router.get('/search', function(req, res, next) {
     res.render('repo', {error: 'Please enter a search term'});
   } else {
     var request = new Request(
-      '/search/repositories?q='+query+'+in:name+user:' + config.githubUser,
+      '/search/repositories?q='+ query +'+in:name+user:' + config.githubUser,
       'GET',
-      {'Authorization': 'token ' + req.signedCookies.accessToken},
+      {Authorization: 'token ' + req.signedCookies.accessToken},
       null
     );
 
     request.do(function(error, response, body) {
       if (error) {
-        res.status(500).send(JSON.stringify({"error": error}))
+        res.render('error', {
+          message: error,
+          error: {
+            status: 500
+          }
+        });
       }
 
       if (response.statusCode == 200) {
         var repoNames = [];
         var parsedRepos = JSON.parse(body).items;
-        for(var i = 0; i < parsedRepos.length; i++) {
+        for (var i = 0; i < parsedRepos.length; i++) {
           repoNames.push(parsedRepos[i].full_name);
         }
         res.render('repo', {repoNames: repoNames});
       } else {
-        res.status(response.statusCode).send(body);
+        res.render('error', {
+          message: JSON.parse(body).message,
+          error: {
+            status: response.statusCode,
+          }
+        });
       }
     });
   }
