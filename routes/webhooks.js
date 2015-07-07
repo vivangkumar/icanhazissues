a7fe747773cbd517ff33cce79f4766e1a29c7766
Vivan Kumar <vivangkumar@gmail.com>
2015-07-07 10:47:31 +0100

    Add webhook route and handler

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var config = CONFIG;
+var pusher = PUSHER;
 
-function handleEvent (body) {
-  var eventMap = {
-    reopened: function(body) {
-
-    }
-  }
-
-  eventMap[body.action](body);
+/**
+ * Use pusher to broadcast changes to frontend
+ */
+function githubSync(owner, repo, event, data) {
+  var channelName = owner + '-' + repo + '-' + 'githubsync';
+  pusher.trigger(channelName, event, data);
 }
 
 router.post('/', function(req, res, next) {
   var body = req.body;
-  handleEvent(body);
+  var repo = req.body.repository.name;
+  var owner = req.body.repository.owner.login;
+  var event = body.action;
+
+  githubSync(owner, repo, event, body);
+  res.status(200).end();
 });
 
 module.exports = router;
